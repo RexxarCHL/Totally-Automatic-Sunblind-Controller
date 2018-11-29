e71e50683b90b4f4ec9da185442133985a5b367a
clin110 <clin110@jhu.edu>
2018-11-29 13:35:34 -0500

    Add light sensor code. Add combined header file

--- check_occupancy_sensor.ino
+++ check_occupancy_sensor.ino
@@ -1,10 +1,10 @@
-// TODO: Change these pin numbers
-const int PIR_ENABLE = 3;
-const int PIR_PIN = 2;
+#include "tasc.h"
 
 void setup_pir_sensor() {
     // Set the pin modes
     pinMode(PIR_PIN, INPUT);
+
+    Serial.println("PIR sensor initialized");
 }
 
 bool check_pir_sensor() {
